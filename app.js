c2a649a4263ef51f47a375260c6d9b6ba8afd22e
sim <simotrone@gmail.com>
2020-09-26 16:11:20 +0200

    a new route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World! (Maybe)'));
+app.get('/name', (req, res) => res.send('Hi there, Marcello!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
